bffa5f08f9f74003292724dd6e35d64788118003
Chely Ho <adept_inept@yahoo.com>
2021-02-03 04:25:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function mergeSort(arr) {
     const midPoint = Math.floor(arr.length / 2);
     const firstArr = arr.slice(0, midPoint);
     const secondArr = arr.slice(midPoint + 1, arr.length - 1);
-    return merge(mergeSort(firstArr, secondArr));
+    return mergeSort(firstArr, secondArr));
   }
 }
 
